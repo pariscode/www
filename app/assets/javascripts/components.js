d897c5cb8fa0f3a76b522b0f2185264554e999de
Sebastien Saunier <seb@saunier.me>
2015-10-06 17:57:42 +0200

    Ask for phone number to applicant

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,6 +1,7 @@
 //= require js-routes
 //= require classnames
 //= require pubsub-js
+//= require lodash
 
 //= require_tree ./helpers
 //= require_tree ./components
--- app/assets/javascripts/components/apply_form_row.jsx
+++ app/assets/javascripts/components/apply_form_row.jsx
@@ -1,38 +1,36 @@
 class ApplyFormRow extends React.Component {
   render() {
-    var row = this.props;
-
-
-    if (row.type == 'input') {
+    if (_.includes(['text', 'phone', 'tel', 'email'], this.props.type)) {
       return(
-        <div
-          className="apply-form-row"
-        >
+        <div className="apply-form-row">
           <label>
-            <i className={row.icon}></i>{row.label}
+            <i className={this.props.icon}></i>{this.props.label}
           </label>
           <input
-            ref='title'
             required={true}
-            placeholder={row.placeholder}
-            name='resource[title]' />
+            placeholder={this.props.placeholder}
+            type={this.props.type}
+            name={this.name()} />
         </div>
       )
-    } else if (row.type == 'text') {
+    } else if (this.props.type === 'textarea') {
       return(
-        <div
-          className="apply-form-row"
-        >
+        <div className="apply-form-row">
           <label>
-            <i className={row.icon}></i>{row.label}
+            <i className={this.props.icon}></i>{this.props.label}
           </label>
           <textarea
             required={true}
-            ref='title'
-            placeholder={row.placeholder}
-            name={'apply{' + row.param + ']'} />
+            placeholder={this.props.placeholder}
+            name={this.name()} />
         </div>
       )
+    } else {
+      throw `Not implemented type: ${this.props.type}`;
     }
+  }
+
+  name() {
+    return `apply[${this.props.param}]`;
   }
 }
