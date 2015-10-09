2843a879744eb099d0a240af4a121120a195b0b6
Thomas Chretien <thomas@chretien.co>
2015-10-09 17:49:49 +0200

    fix bug

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -20,7 +20,7 @@ class AppliesController < ApplicationController
   private
 
   def prepare_apply_form
-    @cities = @client.cities.select{|city| !city['batches'].empty? }.each do |city|
+    @cities = client.cities.select{|city| !city['batches'].empty? }.each do |city|
       city['batches'].sort!{|batch| batch['starts_at'].to_date}.reverse!.each do |batch|
         batch['starts_at'] = batch['starts_at'].to_date.strftime('%B %d')
         batch['ends_at'] = batch['ends_at'].to_date.strftime('%B %d')
