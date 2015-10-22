b25b8a0c145581c8f96e2df40cc0044a01c27d82
Sebastien Saunier <seb@saunier.me>
2015-10-22 18:30:07 +0200

    Test home

--- Gemfile
+++ Gemfile
@@ -56,6 +56,8 @@ group :development, :test do
   gem "pry-byebug"
   gem "pry-rails"
   gem "letter_opener"
+  gem "rspec-rails"
+  gem "capybara"
 end
 
 group :production do
---
+++ spec/features/home_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe "Home", type: :feature do
+  it "works in French" do
+    visit '/'
+    expect(page).to have_selector("h1", text: "apprenez à coder")
+  end
+  it "works in English" do
+    visit '/en'
+    expect(page).to have_selector("h1", text: "learn to code")
+  end
+end
