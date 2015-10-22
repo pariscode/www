e17ca433f45a43784a0c38207122964ef48f8775
Sebastien Saunier <seb@saunier.me>
2015-10-22 12:43:02 +0200

    Meta information on all pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
   def image_url_with_fallback(image_url)
     if image_url.strip.empty?
-      image_path 'social/facebook_card.jpg'
+      image_path 'social/home_facebook_card.png'
     else
       image_url.strip
     end
   end
+
   def markdown(content)
     return "" if content.blank?
     @markdown ||= (
